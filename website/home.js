e6c15e45cd21132ea09ef782572bc973b85422cb
joel <loo.joel93@gmail.com>
2017-02-18 22:13:18 +0800

    Updated website for first check

--- website/home.js
+++ website/home.js
@@ -1,6 +1,4 @@
 $(function() {
-  // This will select everything with the class smoothScroll
-  // This should prevent problems with carousel, scrollspy, etc...
   $('.smoothScroll').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -14,7 +12,3 @@ $(function() {
     }
   });
 });
-
-// Change the speed to whatever you want
-// Personally i think 1000 is too much
-// Try 800 or below, it seems not too much but it will make a difference
