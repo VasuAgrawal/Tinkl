2ace3fda91bbfed439a38ff85e742ee615ccc54c
Vasu Agrawal <vasu_agrawal@yahoo.com>
2017-04-26 20:15:00 -0400

    Streaming data

--- server/grpc_test_client.py
+++ server/grpc_test_client.py
@@ -7,3 +7,7 @@ channel = grpc.insecure_channel("olympus.wv.cc.cmu.edu:5000")
 stub = tinkl_pb2_grpc.TinklStub(channel)
 response = stub.getUrination(PuckId(hub_id=1, sensor_node_id=3))
 print(response)
+
+response = stub.getAllUrination(PuckId(hub_id=1, sensor_node_id=3))
+for sample in response:
+    print(sample)
