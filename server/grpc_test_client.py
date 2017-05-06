023722e442c6dd9d2b8d4ac26a3ce00b5e90faf7
Vasu Agrawal <vasu_agrawal@yahoo.com>
2017-05-06 12:33:22 -0400

    Change to test client

--- server/grpc_test_client.py
+++ server/grpc_test_client.py
@@ -5,9 +5,13 @@ import grpc
 
 channel = grpc.insecure_channel("olympus.wv.cc.cmu.edu:5000")
 stub = tinkl_pb2_grpc.TinklStub(channel)
-response = stub.getUrination(PuckId(hub_id=1, sensor_node_id=3))
+response = stub.getUrination(PuckId(hub_id=1, sensor_node_id=1))
+print("Average Reading:")
 print(response)
+print("-------------------------------------------------")
 
-response = stub.getAllUrination(PuckId(hub_id=1, sensor_node_id=3))
-for sample in response:
+response = stub.getAllUrination(PuckId(hub_id=1, sensor_node_id=1))
+print("Individual Readings:")
+for i, sample in enumerate(response):
+    print("Reading %d" % i)
     print(sample)
